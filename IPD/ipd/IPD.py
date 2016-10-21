65562ba6960b428eb5d83cfc59e6b0b057cb6658
ngocluanbka <ngocluan.bka@gmail.com>
2016-10-21 11:02:54 +0700

    init project

---
+++ IPD/ipd/IPD.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask.templating import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def layout():
+    return render_template('layout.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/home/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run()
